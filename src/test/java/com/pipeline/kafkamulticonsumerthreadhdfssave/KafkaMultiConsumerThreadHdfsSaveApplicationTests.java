73b5236073826998a8c4500f117ab0cf21d7a3d3
weekbelt <vfrvfr4207@gmail.com>
2021-10-27 00:12:23 +0900

    Initial commit

---
+++ src/test/java/com/pipeline/kafkamulticonsumerthreadhdfssave/KafkaMultiConsumerThreadHdfsSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.kafkamulticonsumerthreadhdfssave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMultiConsumerThreadHdfsSaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
